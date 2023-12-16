1d56a2231ff58682b81101d7c9fd8bd32e18fd68
bykowskiolaf <olafbykowski1@gmail.com>
2023-12-16 23:12:01 +0100

    added gh action and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import LandingPage from './pages/LangingPage'
+import MyParticles from './components/Particles'
 
 function App() {
 
 
   return (
     <>
+      <MyParticles />
       <LandingPage />
     </>
   )
--- src/pages/LangingPage/index.tsx
+++ src/pages/LangingPage/index.tsx
@@ -1,14 +1,7 @@
-import MyParticles from "./components/Particles";
-
 const LandingPage = () => {
 
   return (
     <>
-      <MyParticles />
-      <div style={{ display: "flex", position: "relative", justifyItems: "center", justifyContent: "center", width: '100vw', height: '100wh' }}>
-        <h1>Witaj towarzyszu!  </h1>
-        <h5>poklikaj particle</h5>
-      </div>
     </>
   );
 }
